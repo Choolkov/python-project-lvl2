9ae6e91f85896712baee94790bc4d841e1694d04
Choolkov <s.v.chuklov@ya.ru>
2022-05-25 21:22:55 +0300

    add --format argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,9 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('first_file')
 parser.add_argument('second_file')
-
+parser.add_argument(
+    '-f', '--format', metavar='FORMAT', help='set format of output',
+)
 args = parser.parse_args()
 
 
