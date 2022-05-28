082f2d42d6f1e6d71751e01601be074b41294813
Choolkov <s.v.chuklov@ya.ru>
2022-05-28 22:52:21 +0300

    fix test

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -6,8 +6,8 @@ from gendiff.flat import generate_diff
 
 def test_generate_diff():
     """Test for generate_diff function."""
-    first_path = Path('gendiff/tests/fixtures/file1.json')
-    second_path = Path('gendiff/tests/fixtures/file2.json')
-    result_path = Path('gendiff/tests/fixtures/result.txt')
+    first_path = Path(Path.cwd(), 'gendiff/tests/fixtures/file1.json')
+    second_path = Path(Path.cwd(), 'gendiff/tests/fixtures/file2.json')
+    result_path = Path(Path.cwd(), 'gendiff/tests/fixtures/result.txt')
     expected_result = result_path.read_text()
     assert generate_diff(first_path, second_path) == expected_result
