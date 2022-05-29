e4fcb8f8626182e0736acab4edb0b77699b7de41
Choolkov <s.v.chuklov@ya.ru>
2022-05-29 19:24:44 +0300

    fix tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,7 +1,7 @@
 """Test module."""
 from gendiff.flat import generate_diff
 from tests.fixtures_paths import (
-    JSON_DIFF_FILE,
+    FLAT_DIFF_FILE,
     JSON_FIRST_FILE,
     JSON_SECOND_FILE,
     YAML_FIRST_FILE,
@@ -11,6 +11,6 @@ from tests.fixtures_paths import (
 
 def test_generate_diff():
     """Test for generate_diff function."""
-    expected_diff = JSON_DIFF_FILE.read_text()
+    expected_diff = FLAT_DIFF_FILE.read_text()
     assert generate_diff(JSON_FIRST_FILE, JSON_SECOND_FILE) == expected_diff
     assert generate_diff(YAML_FIRST_FILE, YAML_SECOND_FILE) == expected_diff
