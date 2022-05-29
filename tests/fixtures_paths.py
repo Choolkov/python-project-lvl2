a3f073ced23e694fc72f4f7075c0d90e8877ecb0
Choolkov <s.v.chuklov@ya.ru>
2022-05-29 19:13:37 +0300

    add new fixtures

--- tests/fixtures_paths.py
+++ tests/fixtures_paths.py
@@ -4,11 +4,17 @@ from pathlib import Path
 TESTS = Path(__file__).parent
 
 FIXTURES = Path(TESTS, 'fixtures')
+
 JSON_FIRST_FILE = Path(FIXTURES, 'file1.json')
 JSON_SECOND_FILE = Path(FIXTURES, 'file2.json')
+JSON_THIRD_FILE = Path(FIXTURES, 'file3.json')
+JSON_FOURTH_FILE = Path(FIXTURES, 'file4.json')
+
 YAML_FIRST_FILE = Path(FIXTURES, 'file1.yaml')
 YAML_SECOND_FILE = Path(FIXTURES, 'file2.yaml')
-
+YAML_THIRD_FILE = Path(FIXTURES, 'file3.yaml')
+YAML_FOURTH_FILE = Path(FIXTURES, 'file4.yaml')
 
 DIFFS = Path(FIXTURES, 'diffs')
-JSON_DIFF_FILE = Path(DIFFS, 'json_diff.txt')
+FLAT_DIFF_FILE = Path(DIFFS, 'flat_diff.txt')
+RECURSIVE_DIFF_FILE = Path(DIFFS, 'recursive_diff.txt')
